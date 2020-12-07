cdd418be5a18fd7da647f7d9d7245ade6b3e97ed
João Matos <joao.g.matos@inesctec.pt>
2020-12-07 02:28:29 +0000

    Add CSS files for Index and About

--- about.php
+++ about.php
@@ -1,75 +1,84 @@
 <!DOCTYPE html>
-<?php
-        include('templates/header_public.html');
-    ?>
+<?php include('templates/head.html'); ?>
+<link href="css/style_about.css" rel="stylesheet">
+<link href="css/layout_about.css" rel="stylesheet">
+<?php include('templates/header_public.html'); ?>
 
 <br>
 
-<section>
-    <h1> OUR PHILOSOPHY</h1>
-    <div>
-        <h2>Participants, Speakers, Staff, </h2>
-        <h2>Sponsors, Partners <b>together</b></h2>
+<h1 id="titlePhilosophy"> OUR PHILOSOPHY</h1>
 
-        <p>
-            Most event organizers are faced with the need to work with loads of different technologies for the
-            management of people and entities who participate in their events.
-        </p>
-        <p>
-            cBooking gathers them all in a single plataform, providing the organizer with tools to register, track
-            and manage whoever is going to participate in their event.
-        </p>
-    </div>
-    <div>
-        <h2>A <b>free</b> and <b>transparent</b> tool </h2>
-        <h2> to support event organizers </h2>
-        <p>
-            At cBooking we work to deliver a free tool that can promote the organization of events, by
-            supporting its logistics and registrations processes.
-        </p>
-        <p>
-            Whoever is participating in an event can register himself and reach other available events.
-            To make it as smooth as possible for you, dear organizer: we process your data, track registrations,
-            and provide you with a private dashboard for the management of the event.
-        </p>
-    </div>
+<section id="text1">
+    <h2>Participants, Speakers, Staff, Sponsors, Partners <b>together</b></h2>
+    <p>
+        Most event organizers are faced with the need to work with loads of different technologies for the
+        management of people and entities who participate in their events.
+    </p>
+    <p>
+        cBooking gathers them all in a single plataform, providing the organizer with tools to register, track
+        and manage whoever is going to participate in their event.
+    </p>
 </section>
-<section>
-    <h1> WHO WE ARE</h1>
-    <h2> Two <b>Biomedical Engineering</b> Students,</h2>
-    <h2> learning how to construct a website</h2>
-    <div id="Peralta">
-        <img src="images/peralta.png" alt="Guilherme Peralta">
-        <div>
-            <p><b>Guilherme Peralta</b></p>
-            <p>Ovar, Portugal</p>
-            <p>21 years old</p>
-            <a href="https://www.linkedin.com/in/guilherme-peralta-677345b8/" target="_blank">
-                <img src="images/linkedin.png" alt="linkedin logo" width=50>
-            </a>
-            <a href="https://github.com/Guilherme9909" target="_blank">
-                <img src="images/github.png" alt="github logo" width=50>
-            </a>
-        </div>
 
-    </div>
-    <br>
-    <div id="Matos">
-        <img src="images/matos.png" alt="João Matos">
-        <div>
-            <p><b>João Matos</b></p>
-            <p>Porto, Portugal</p>
-            <p>20 years old</p>
-            <a href="https://www.linkedin.com/in/joao-mat0s/" target="_blank">
-                <img src="images/linkedin.png" alt="linkedin logo" width=50>
-            </a>
-            <a href="https://github.com/joamats" target="_blank">
-                <img src="images/github.png" alt="github logo" width=50>
-            </a>
-        </div>
-
-    </div>
+<section id="img1">
+    <img src="images/together.png" alt="together image">
 </section>
+
+
+<section id="text2">
+    <h2>A <b>free</b> and <b>transparent</b> tool to support event organizers </h2>
+    <p>
+        At cBooking we work to deliver a free tool that can promote the organization of events, by
+        supporting its logistics and registrations processes.
+    </p>
+    <p>
+        Whoever is participating in an event can register himself and reach other available events.
+        To make it as smooth as possible for you, dear organizer: we process your data, track registrations,
+        and provide you with a private dashboard for the management of the event.
+    </p>
+</section>
+
+<section id="img2">
+    <img src="images/transparency.png" alt="transparency image">
+</section>
+
+
+
+    <h1 id="titleContacts"> WHO WE ARE</h1>
+    <h2 id="titleUs"> Two <b>Biomedical Engineering</b> Students, learning how to construct a website</h2>
+
+
+
+<section id="Peralta">
+    <img src="images/peralta.png" alt="Guilherme Peralta">
+    <p><b>Guilherme Peralta</b></p>
+    <p>Ovar, Portugal</p>
+    <p>21 years old</p>
+    <a href="https://www.linkedin.com/in/guilherme-peralta-677345b8/" target="_blank">
+        <img src="images/linkedin.png" alt="linkedin logo" width=50>
+    </a>
+    <a href="https://github.com/Guilherme9909" target="_blank">
+        <img src="images/github.png" alt="github logo" width=50>
+    </a>
+
+</section>
+
+<br>
+
+<section id="Matos">
+    <img src="images/matos.png" alt="João Matos">
+    <p><b>João Matos</b></p>
+    <p>Porto, Portugal</p>
+    <p>20 years old</p>
+    <a href="https://www.linkedin.com/in/joao-mat0s/" target="_blank">
+        <img src="images/linkedin.png" alt="linkedin logo" width=50>
+    </a>
+    <a href="https://github.com/joamats" target="_blank">
+        <img src="images/github.png" alt="github logo" width=50>
+    </a>
+</section>
+
+<br>
 
 <?php
         include('templates/footer.html');
