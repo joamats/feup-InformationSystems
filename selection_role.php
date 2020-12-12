28e22a8902cc1e84720e539b55a31f68575e6796
Guilherme Peralta <up201704277@fe.up.pt>
2020-12-12 13:17:54 +0000

    Created Selection_role.css and altered head on selection_role.php

--- selection_role.php
+++ selection_role.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
+<?php include('templates/head.html'); ?>
 <?php
     include('templates/header_public.html');
 ?>
+<link href="css/style_selection_role.css" rel="stylesheet">
 
-<h2>Registration for Event: What's your Role?</h2>
+<h2>Registration for Event:</h2>
+<h3>What's your Role?</h3>
 <ul>
     <li>
         <a href="registration_participant.php" >Participant</a>
