659aaa2df59d9ac5068669f8df7ce3efac34af2c
João Matos <joao.g.matos@inesctec.pt>
2020-12-20 22:00:54 +0000

    Add CSS design to event details Registered users (closes #26)

--- templates/event_details.php
+++ templates/event_details.php
@@ -11,76 +11,91 @@
         <div id="leftText">
             <h2>About this Event</h2>
             <p><?=$eventInfo['aboutEvent']?></p>
-        
-            <?php if(!empty($eventSpeakers)) {?>
-            <h2>The Speakers</h2>
-            <?php foreach($eventSpeakers as $speaker){
-                $speakerId = $speaker['id']; ?>
 
-            <div class = "picFrame">
-                <img class = "profilePic" src="images/persons/<?=$speaker['profile_pic']?>" alt=<?=$speaker['name']?> width=100>
-            </div>
-            <p>
-                <?=$speaker['title']?>   <?=$speaker['name']?><br>
-                <?=$speaker['talk_subject']?><br>
-                <a href="see_more.php?eventId=<?=$eventId?>&item=Speaker&personId=<?=$speakerId?>">Read more</a>
-            </p>
-
-            <?php } }
             
-            if(!empty($eventSponsors)) {?>
+            <section class = "listRegistered">
+                <?php if(!empty($eventSpeakers)) {?>
+                    <h2>The Speakers</h2><br>
+                    <?php foreach($eventSpeakers as $speaker){
+                        $speakerId = $speaker['id']; ?>
+                        <div class = "listItem">
+                            <div class = "picFrame">
+                                <img class = "profilePic" src="images/persons/<?=$speaker['profile_pic']?>" alt=<?=$speaker['name']?> width=100>
+                            </div>
+                            <p>
+                                <b><?=$speaker['title']?>   <?=$speaker['name']?></b><br>
+                                <?=$speaker['talk_subject']?><br>
+                                <a href="see_more.php?eventId=<?=$eventId?>&item=Speaker&personId=<?=$speakerId?>">
+                                    Read more
+                                    <i class="fas fa-arrow-right"></i>
+                                </a>
+                            </p>
+                        </div>
+                    
+
+                <?php } } ?>
+            </section>
+
+            <section class = "listRegistered">
+            <?php if(!empty($eventSponsors)) {?>
 
             <h2>The Sponsors</h2>
             <?php foreach($eventSponsors as $sponsor){
                 $sponsorId = $sponsor['id']; ?>
+                <div class = "listItem">
+                    <a href="<?=$sponsor['website_link']?>" target="_blank">
+                        <img class="logotype" src="images/entities/<?=$sponsor['logotype']?>" alt=<?=$sponsor['name']?> width = 125>
+                    </a>
+                </div>
+            <?php } } ?>
+            </section>
 
-            <p>
-                <?=$sponsor['name']?><br>
-                <a href="<?=$sponsor['website_link']?>" target="_blank">
-                    <img class="logotype" src="images/entities/<?=$sponsor['logotype']?>" alt=<?=$sponsor['name']?> width = 100>
-                </a>
-            </p>
-
-            <?php } 
-            }
-            if(!empty($eventPartners)) { ?>
+            <section class = "listRegistered">
+            <?php if(!empty($eventPartners)) { ?>
 
             <h2>The Partners</h2>
             <?php foreach($eventPartners as $partner){
                 $partnerId = $partner['id']; ?>
-                
-                <p>
-                <?=$partner['name']?><br>
-                <a href="<?=$partner['website_link']?>" target="_blank">
-                    <img class="logotype" src="images/entities/<?=$partner['logotype']?>" alt=<?=$partner['name']?> width = 100>
-                </a>
-                </p>
+                <div class = "listItem">
+                    <a href="<?=$partner['website_link']?>" target="_blank">
+                        <img class="logotype" src="images/entities/<?=$partner['logotype']?>" alt=<?=$partner['name']?> width = 125>
+                    </a>
+                </div>
+        
+            <?php } } ?>
+            </section>
 
-            <?php } 
-            }
-            if(!empty($eventStaff)) { ?>
+            <section class = "listRegistered">
+                <?php if(!empty($eventStaff)) { ?>
 
-            <h2>The Staff</h2>
-            <?php foreach($eventStaff as $staff){
-                $staffId = $staff['id']; ?>
-
-            <img class="profilePic" src="images/persons/<?=$staff['profile_pic']?>" alt=<?=$staff['name']?> width = 100>
-            <p>
-                <?=$staff['name']?><br>
-                Department: <?=$staff['department']?><br>
-            </p>
-            <?php } }?>
+                <h2>The Staff</h2>
+                <?php foreach($eventStaff as $staff){
+                    $staffId = $staff['id']; ?>
+                    <div class = "listItem">
+                        <div class = "picFrame">
+                            <img class="profilePic" src="images/persons/<?=$staff['profile_pic']?>" alt=<?=$staff['name']?> width = 100>
+                        </div>
+                        <p>
+                            <b><?=$staff['name']?></b><br><br>
+                            Department: <?=$staff['department']?><br>
+                        </p>
+                    </div>
+                <?php } }?>
+            </section>
 
             <h2>Event by:</h2>
-            <img class="logotype" src="images/persons/<?=$eventOrganizer['logotype']?>" alt=<?=$eventOrganizer['name']?> width = 100>
-            <p>
-                <?=$eventOrganizer['name']?><br>
-                Email: <?=$eventOrganizer['email']?><br>
-                Phone Number: <?=$eventOrganizer['phone_num']?><br>
-                Address: <?=$eventOrganizer['address']?><br>
-                VAT Number: <?=$eventOrganizer['vat_num']?><br>
-            </p>
-        </div>
+            <div class = "listItem">    
+                <div class = "picFrame">
+                    <img class="profilePic" src="images/persons/<?=$eventOrganizer['logotype']?>" alt=<?=$eventOrganizer['name']?> width = 100>
+                </div>
+                <p>
+                    <b><?=$eventOrganizer['name']?></b><br>
+                    Email: <?=$eventOrganizer['email']?><br>
+                    Phone Number: <?=$eventOrganizer['phone_num']?><br>
+                    Address: <?=$eventOrganizer['address']?><br>
+                    VAT Number: <?=$eventOrganizer['vat_num']?><br>
+                </p>
+            </div>
     </section> <!-- leftPanel -->
 
     <section id = "rightPanel">
