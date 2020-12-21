fd80669d569370e115e051e4604acc18cbbda4ca
Guilherme Peralta <up201704277@fe.up.pt>
2020-12-21 21:55:32 +0000

    css image upload page

--- templates/images.php
+++ templates/images.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 
+<link href="css/style_images.css" rel="stylesheet">
+<link href="css/layout_images.css" rel="stylesheet">
+
 <br>
 <h1><?=$title?></h1>
+
 
 <?php if (isset($_MESSAGE)) { ?>
     <div class="errorMessage">
@@ -10,7 +14,10 @@
 <?php } ?>
 
 <form action="action_imagesUpload.php" method="post" enctype="multipart/form-data">
-    Upload <?=$prompt?> *:
-    <input type="file" name="fileToUpload" id="fileToUpload" required>
-    <input type="submit" value="Submit" name="submit">
+    <label>Upload <?=$prompt?> *
+        <br>
+        <input id= "fileToUpload" type="file" name="fileToUpload" id="fileToUpload" required>
+        <br>
+        <input id="submit_button" type="submit" value="Submit" name="submit">
+    </label>
 </form>
